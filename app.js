f1be4f0f3f7131fdb7689e29ab6ae8de503e871f
Aman <amanfarhan2008@gmail.com>
2025-07-11 11:31:12 +0500

    Javascript lab Class Added

---
+++ app.js
@@ -0,0 +1,48 @@
+// let arrey = ["karachi","islamabad","pishawer","multan"];
+
+// console.log(arrey.indexOf("islamabad","multan"))
+// arrey.splice(2,0,"pindi","panjab");
+// arrey.pop()
+// console.log(arrey);
+
+
+// let num1 = prompt("enter your number");
+// let num2 = prompt("enter your number");
+
+// if (num1 === num2){
+//     console.log(num1+ " equals "+ num2);
+// } else {
+//     console.log("both are not equal")
+// }
+
+
+// let num1 = prompt("enter your number");
+
+// if (num1 >= 10 && num1 <= 20  ){
+//     console.log("this is equal");
+// }else{
+//     console.log("are not ");
+// }
+
+// let password = prompt("enter a num")
+// let print = document.getElementById("text");
+
+// if (password.length <= 6){
+//    print.innerHTML = "are you accepted"
+// }
+// else {
+//     print.innerHTML = "password exceeds the limit"
+// }
+
+
+// let password = prompt("enter a num");
+
+// let print = document.getElementById("text");
+
+// if (password %2 === 0 && password > 100 ){
+//     print.innerHTML = "the number is even and above 100";
+// } else if (password % 2 !== 0 && password > 100){
+//     print.innerHTML = "the number is odd and above 100"
+// }else {
+//     print.innerHTML = "this number is less then 100"
+// }
