7bb73822d734feea3a93b370921b69bfe0002b3c
Aman <amanfarhan2008@gmail.com>
2025-07-12 23:11:32 +0500

    sssssssssss

--- app.js
+++ app.js
@@ -46,3 +46,53 @@
 // }else {
 //     print.innerHTML = "this number is less then 100"
 // }
+// let i = 1 ;
+// for( i = 1 , i <= 5 , i++) {
+//     console.log("aman")
+// }
+//     console.log("")
+// }
+
+
+
+
+// let i = 1;
+// for( i = 1 ; i <= 5; i++) {
+//     console.log("aman")
+// } 
+
+
+// let i = 1;
+// for( i = 1 ; i <= 5 ; i++ ) {
+//     console.log("aman");
+// }
+
+
+// let i = 5 ;
+// for (i = 1 ; 1 >= 10 ; i++){
+//     console.log(i + " x " + i +"=" + i*i );
+// }
+
+
+
+// let sum = 0 ;
+// for(let i = 1 ; i <= 5; i++){
+//     sum = sum + i;
+// };
+// console.log("sum = " + sum );
+// let a = 0
+// for(let i = 1 ; i <= 10 ; i ++){
+//     a  = a+i ;
+//     console.log(i);
+// }
+// console.log("a = ", a);
+// console.log("loop was endeed");
+
+
+
+let sum = 0;
+for(let i = 1 ; i <= 10; i++){
+    sum = sum + i 
+    console.log(i);  
+};
+console.log(sum);
